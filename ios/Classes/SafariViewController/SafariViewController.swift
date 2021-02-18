e0dccdebd2669cf3b0ac03fd025a1bf060ae714d
dagba <dagdk95@gmail.com>
2021-02-18 22:59:31 +0800

    - remove multiple navbar offset

--- ios/Classes/SafariViewController/SafariViewController.swift
+++ ios/Classes/SafariViewController/SafariViewController.swift
@@ -72,16 +72,16 @@ public class SafariViewController: SFSafariViewController, FlutterPlugin, SFSafa
     public override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        var frame = view.frame
-        let OffsetY: CGFloat = 56
-        if (UIScreen.main.bounds.height > 667) {
-            frame.origin = CGPoint(x: frame.origin.x, y: frame.origin.y - OffsetY)
-            frame.size = CGSize(width: frame.width, height: frame.height + (OffsetY * 2.8))
-        } else {
-            frame.origin = CGPoint(x: frame.origin.x, y: frame.origin.y - OffsetY)
-            frame.size = CGSize(width: frame.width, height: frame.height + (OffsetY * 2))
-        }
-        view.frame = frame
+//        var frame = view.frame
+//        let OffsetY: CGFloat = 56
+//        if (UIScreen.main.bounds.height > 667) {
+//            frame.origin = CGPoint(x: frame.origin.x, y: frame.origin.y - OffsetY)
+//            frame.size = CGSize(width: frame.width, height: frame.height + (OffsetY * 2.8))
+//        } else {
+//            frame.origin = CGPoint(x: frame.origin.x, y: frame.origin.y - OffsetY)
+//            frame.size = CGSize(width: frame.width, height: frame.height + (OffsetY * 2))
+//        }
+//        view.frame = frame
     }
 
 
